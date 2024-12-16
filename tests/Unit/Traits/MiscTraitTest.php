3e317a56183812ddf4cd10f4e37caa7f49936fad
Ivan Chistyakov <vancodx@gmail.com>
2024-12-16 23:19:19 +0300

    Added MiscTraitTest.php

---
+++ tests/Unit/Traits/MiscTraitTest.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit\Traits;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use Tests\Unit\ValidationTestCase;
+use VanCodX\Data\Validation\Validation as V;
+
+class MiscTraitTest extends ValidationTestCase
+{
+    /**
+     * @return mixed[][]
+     * @phpstan-return array<int, array{0: mixed, 1: bool}>
+     */
+    public static function isNullDataProvider(): array
+    {
+        return static::buildDataProvider([
+            null
+        ]);
+    }
+
+    /**
+     * @param mixed $value
+     * @param bool $expected
+     * @return void
+     */
+    #[DataProvider('isNullDataProvider')]
+    public function testIsNull(mixed $value, bool $expected): void
+    {
+        $this->assertSame($expected, V::isNull($value));
+    }
+}
