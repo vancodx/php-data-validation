459a535913657c4d0af3c2cfa26f401292cca7ee
Ivan Chistyakov <vancodx@gmail.com>
2024-12-25 22:38:02 +0300

    Added BasicTraits.php

---
+++ src/Traits/BasicTraits.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace VanCodX\Data\Validation\Traits;
+
+use VanCodX\Data\Validation\Traits\Basic\ArrTrait;
+use VanCodX\Data\Validation\Traits\Basic\BoolTrait;
+use VanCodX\Data\Validation\Traits\Basic\FloatTrait;
+use VanCodX\Data\Validation\Traits\Basic\IntTrait;
+use VanCodX\Data\Validation\Traits\Basic\ListTrait;
+use VanCodX\Data\Validation\Traits\Basic\MiscTrait;
+use VanCodX\Data\Validation\Traits\Basic\NumTrait;
+use VanCodX\Data\Validation\Traits\Basic\StrTrait;
+
+trait BasicTraits
+{
+    use BoolTrait;
+    use IntTrait;
+    use FloatTrait;
+    use NumTrait;
+    use StrTrait;
+    use MiscTrait;
+    use ArrTrait;
+    use ListTrait;
+}
--- src/Validation.php
+++ src/Validation.php
@@ -3,26 +3,12 @@
 namespace VanCodX\Data\Validation;
 
 use VanCodX\Data\Validation\Traits\ArrOfTraits;
-use VanCodX\Data\Validation\Traits\ArrTrait;
-use VanCodX\Data\Validation\Traits\BoolTrait;
-use VanCodX\Data\Validation\Traits\FloatTrait;
-use VanCodX\Data\Validation\Traits\IntTrait;
-use VanCodX\Data\Validation\Traits\ListTrait;
-use VanCodX\Data\Validation\Traits\MiscTrait;
-use VanCodX\Data\Validation\Traits\NumTrait;
+use VanCodX\Data\Validation\Traits\BasicTraits;
 use VanCodX\Data\Validation\Traits\OrNullTraits;
-use VanCodX\Data\Validation\Traits\StrTrait;
 
 class Validation
 {
-    use ArrTrait;
-    use BoolTrait;
-    use FloatTrait;
-    use IntTrait;
-    use ListTrait;
-    use MiscTrait;
-    use NumTrait;
-    use StrTrait;
+    use BasicTraits;
     use OrNullTraits;
     use ArrOfTraits;
 }
