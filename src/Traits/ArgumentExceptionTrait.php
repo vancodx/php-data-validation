19afa13c46469e541b9ed6f620fa0c3d7b4bd766
Ivan Chistyakov <vancodx@gmail.com>
2025-02-06 21:44:10 +0300

    Fixed ArgumentExceptionTrait.php

--- src/Traits/ArgumentExceptionTrait.php
+++ src/Traits/ArgumentExceptionTrait.php
@@ -4,6 +4,7 @@ namespace VanCodX\Data\Validation\Traits;
 
 use Exception;
 use InvalidArgumentException;
+use Throwable;
 
 trait ArgumentExceptionTrait
 {
@@ -31,11 +32,36 @@ trait ArgumentExceptionTrait
     }
 
     /**
+     * @param class-string<Exception> $class
+     * @return void
+     */
+    public static function setArgumentExceptionClass(string $class): void
+    {
+        /** @phpstan-ignore staticMethod.alreadyNarrowedType */
+        if (!static::isClsOf($class, Exception::class)) {
+            throw new InvalidArgumentException('Argument "class" is invalid.');
+        }
+        static::$argumentExceptionClass = $class;
+    }
+
+    /**
      * @return string
      */
     public static function getSingularArgumentMessageFormat(): string
     {
         return static::$singularArgumentMessageFormat;
+    }
+
+    /**
+     * @param string $format
+     * @return void
+     */
+    public static function setSingularArgumentMessageFormat(string $format): void
+    {
+        if (!static::isStrLen($format)) {
+            throw new InvalidArgumentException('Argument "format" is invalid.');
+        }
+        static::$singularArgumentMessageFormat = $format;
     }
 
     /**
@@ -47,23 +73,39 @@ trait ArgumentExceptionTrait
     }
 
     /**
-     * @param string|array<string, mixed> $nameOrValues
+     * @param string $format
+     * @return void
+     */
+    public static function setPluralArgumentMessageFormat(string $format): void
+    {
+        if (!static::isStrLen($format)) {
+            throw new InvalidArgumentException('Argument "format" is invalid.');
+        }
+        static::$pluralArgumentMessageFormat = $format;
+    }
+
+    /**
+     * @param string|array<string, mixed> $argument
+     * @param int $code [optional]
+     * @param Throwable|null $previous [optional]
      * @return Exception
      */
-    public static function newArgumentException(string|array $nameOrValues): Exception
-    {
-        if (static::isStrLen($nameOrValues)) {
-            $message = sprintf(static::getSingularArgumentMessageFormat(), $nameOrValues);
-        } elseif (static::isAssocLen($nameOrValues)) {
-            if (count($nameOrValues) === 1) {
-                $message = sprintf(static::getSingularArgumentMessageFormat(), array_key_first($nameOrValues));
+    public static function newArgumentException(
+        string|array $argument,
+        int $code = 0,
+        Throwable $previous = null
+    ): Exception {
+        if (static::isStrLen($argument)) {
+            $message = sprintf(static::getSingularArgumentMessageFormat(), $argument);
+        } elseif (static::isAssocLen($argument)) {
+            if (count($argument) === 1) {
+                $message = sprintf(static::getSingularArgumentMessageFormat(), array_key_first($argument));
             } else {
-                $message = sprintf(static::getPluralArgumentMessageFormat(), implode(', ', array_keys($nameOrValues)));
+                $message = sprintf(static::getPluralArgumentMessageFormat(), implode(', ', array_keys($argument)));
             }
         } else {
-            throw new InvalidArgumentException('Argument "nameOrValues" is invalid.');
+            throw new InvalidArgumentException('Argument "argument" is invalid.');
         }
-        $argumentExceptionClass = static::getArgumentExceptionClass();
-        return new $argumentExceptionClass($message);
+        return new (static::getArgumentExceptionClass())($message, $code, $previous);
     }
 }
