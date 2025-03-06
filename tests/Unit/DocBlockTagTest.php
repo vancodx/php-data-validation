9c2f6b4fd7a8cba93b6fbc0b2f7d386d06bdda72
Ivan Chistyakov <vancodx@gmail.com>
2025-03-06 22:40:16 +0300

    Fixed DocBlockTagTest.php

--- tests/Unit/DocBlockTagTest.php
+++ tests/Unit/DocBlockTagTest.php
@@ -102,8 +102,8 @@ class DocBlockTagTest extends TestCase
             $docComment = $method->getDocComment();
             return [
                 $method->getName(),
-                $docComment && preg_match('~@phpstan-assert-if-true (?<type>[^$]+) \$value\n~', $docComment, $match)
-                    ? $match['type'] : null
+                $docComment && preg_match('~@phpstan-assert-if-true ([^$]+) \$value\n~', $docComment, $match)
+                    ? $match[1] : null
             ];
         }, $methods);
     }
